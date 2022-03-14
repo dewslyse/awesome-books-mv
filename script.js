93d27843d9fff9a943ce3e189c9097189a5386e7
dewslyse <dewslyse@gmail.com>
2022-03-14 20:13:16 +0000

    Add new book to localStorage

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 const addBtn = document.querySelector('.add-btn');
-const bookTitle = document.getElementById('title').value;
-const bookAuthor = document.getElementById('author').value;
 
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
-}
+let awesomeBooks = [];
 
-addBtn.addEventListener('click', () => {
-  const newBook = new Book(bookTitle, bookAuthor);
-  console.log(newBook);
+newBook = {};
+
+// function Book(title, author) {
+//   this.title = title;
+//   this.author = author;
+// }
+
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const bookTitle = document.getElementById('title').value;
+  const bookAuthor = document.getElementById('author').value;
+
+  newBook = {
+    title: bookTitle,
+    author: bookAuthor
+  };
+
+  localStorage.setItem('book', JSON.stringify(newBook));
+  awesomeBooks = JSON.parse(localStorage.getItem('awesomebook'));
+
+  awesomeBooks.push({
+    Title: bookTitle,
+    Author: bookAuthor
+  });
+
+  localStorage.setItem('awesomebook', JSON.stringify(newBook));
+
+
+
+
+
+
+
+  // let addedBook = localStorage.getItem('addedBook');
+  // if (addedBook === null) {
+  //   awesomeBooks = [];
+  // }
+  // else {
+  //   addedBook = JSON.parse(addBook);
+  // }
+
+  // awesomeBooks.push(newBook);
+  // const newBook = new Book(bookTitle, bookAuthor);
+  // console.log(newBook);
+
 });
